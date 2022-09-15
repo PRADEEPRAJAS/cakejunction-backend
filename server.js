359bd2068f75785fda59cdad2f76c9d42a0647cf
Pradeep Raja <100185778+PRADEEPRAJAS@users.noreply.github.com>
2022-09-15 12:14:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = require("./app");
 
 app.get("/",()=>{
-res.end("hello")}
+res.send("hello")}
 const DB =
   "mongodb+srv://pradeepraja:95666@trainingcluster.b8jho.mongodb.net/operationProduct?retryWrites=true&w=majority";
 mongoose.connect(DB).then(() => {
